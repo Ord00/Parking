c47b6a23b2adb9e0f834e45867d9d0345a915b2d
Ord00 <ivansimonov7@yandex.ru>
2025-10-09 16:51:43 +0300

    Added user generator build gradle file

---
+++ UserGenerator/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("org.springframework.boot")
+    id("com.google.protobuf")
+}
+
+val grpcVersion = "1.59.0"
+val protobufVersion = "4.28.2"
+val protocVersion = protobufVersion
+
+dependencies {
+    implementation(project(":Model"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.grpc:grpc-netty:${grpcVersion}")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("com.google.protobuf:protobuf-java:${protobufVersion}")
+    implementation("com.google.protobuf:protobuf-java-util:${protobufVersion}")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${protocVersion}"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/grpc",
+                "build/generated/source/proto/main/java"
+            )
+        }
+    }
+}
